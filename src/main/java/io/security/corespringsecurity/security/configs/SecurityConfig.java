a0c2d31f6fcc826c6ac0db376889de09963e1e52
hwiyeon0831 <aab555586@gmail.com>
2023-03-28 01:25:26 +0900

    3)-4 내용 등록 - UserDetailService를 구현한 CustomUserDetailService를 통해 DB에 있는 사용자를 통해 권한을 부여하도록 설정 변경 - CustomUserDetailService 생성 및 구현 - DB 정보가 저장되어 있는 Account 객체를 통해 UserDetail타입으로 변환하기 위한 AccountContext 생성 및 구현 - 권한 부여 설정 변경

--- src/main/java/io/security/corespringsecurity/security/configs/SecurityConfig.java
+++ src/main/java/io/security/corespringsecurity/security/configs/SecurityConfig.java
@@ -1,5 +1,6 @@
 package io.security.corespringsecurity.security.configs;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,6 +9,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -15,42 +17,55 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        // passwordEncoder를 통해 암호화한 방식으로 전달
-        String password = passwordEncoder().encode("1111");
 
-        //인메모리 방식으로 사용자 생성. 권한 부여.
-        auth.inMemoryAuthentication().withUser("user").password(password).roles("USER");
-        auth.inMemoryAuthentication().withUser("manager").password(password).roles("MANAGER", "USER");
-        auth.inMemoryAuthentication().withUser("admin").password(password).roles("ADMIN", "MANAGER", "USER"); // 위 설정으로는 권한에 대한 계층 구조를 Spring Security에서 파악하지 못한다. -> 추후 실전 프로젝트에서 학습 예정
-    }
+	@Autowired
+	private UserDetailsService userDetailsService;
 
-    @Bean   //passwordEncoder는 빈으로 등록
-    public PasswordEncoder passwordEncoder() {
-        //평문인 password를 암호화처리
-        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
-    }
+	// DB에 있는 사용자를 조회하여 권한 부여할 것이므로 인메모리 방식 설정 삭제
+//	@Override
+//	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//		// passwordEncoder를 통해 암호화한 방식으로 전달
+//		String password = passwordEncoder().encode("1111");
+//
+//		//인메모리 방식으로 사용자 생성. 권한 부여.
+//		auth.inMemoryAuthentication().withUser("user").password(password).roles("USER");
+//		auth.inMemoryAuthentication().withUser("manager").password(password).roles("MANAGER", "USER");
+//		auth.inMemoryAuthentication().withUser("admin").password(password).roles("ADMIN", "MANAGER", "USER"); // 위 설정으로는 권한에 대한 계층 구조를 Spring Security에서 파악하지 못한다. -> 추후 실전 프로젝트에서 학습 예정
+//	}
 
-    @Override
-    public void configure(WebSecurity web) throws Exception {
-        //permitAll()과 다른 점 : permitAll()은 해당 요청에 대해 허가는 하되 보안 필터는 거친다. WebIgnore은 명시한 자원에 대해 아예 보안 필터를 거치지 않도록 한다.
-        web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
-    }
+	//CustomUserService를 사용하여 권한을 부여하도록 설정
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-                .authorizeRequests()
-                .antMatchers("/").permitAll()
-                // 페이지별 권한 매칭
-                .antMatchers("/mypage").hasRole("USER")
-                .antMatchers("/messages").hasRole("MANAGER")
-                .antMatchers("/config").hasRole("ADMIN")
-                .anyRequest().authenticated()
-                .and()
-                .formLogin()
-        ;
 
-    }
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(userDetailsService);
+	}
+
+	@Bean   //passwordEncoder는 빈으로 등록
+	public PasswordEncoder passwordEncoder() {
+		//평문인 password를 암호화처리
+		return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+	}
+
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		//permitAll()과 다른 점 : permitAll()은 해당 요청에 대해 허가는 하되 보안 필터는 거친다. WebIgnore은 명시한 자원에 대해 아예 보안 필터를 거치지 않도록 한다.
+		web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+				.authorizeRequests()
+				.antMatchers("/", "/users").permitAll()
+				// 페이지별 권한 매칭
+				.antMatchers("/mypage").hasRole("USER")
+				.antMatchers("/messages").hasRole("MANAGER")
+				.antMatchers("/config").hasRole("ADMIN")
+				.anyRequest().authenticated()
+				.and()
+				.formLogin()
+		;
+
+	}
 }
